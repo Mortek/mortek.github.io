dd18bbbe87aa496f7c25a26bcd4acb491496132e
Maurice Moret <mmoret@netvlies.net>
2026-04-09 22:49:40 +0200

    Style featured book buttons, make playlist links solid buttons, bump caches
    - Increase featured book button margin-top to 10px
    - Remove box-shadow glow on featured book button hover
    - Restyle playlist links as solid amber buttons with hover lift
    - Bump CSS cache to v30, service worker to mortek-v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mortek-v7';
+const CACHE = 'mortek-v8';
 const PRECACHE = [
   '/',
   '/index.html',
@@ -8,7 +8,7 @@ const PRECACHE = [
   '/about.html',
   '/blog.html',
   '/404.html',
-  '/style.min.css?v=29',
+  '/style.min.css?v=30',
   '/favicon.svg',
   '/manifest.json',
   '/apple-touch-icon.png',
