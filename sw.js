3d1a17f67f024a7be0fe96d25d25e39b7429d75a
Maurice Moret <mmoret@netvlies.net>
2026-04-13 18:37:01 +0200

    style: make article buttons solid with darker orange tint
    Change .buy-btn--outline from transparent/outline style to solid
    background (#d97d0a) matching buy buttons but one shade darker.
    Bump cache versions to v=53 / mortek-v32.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mortek-v31';
+const CACHE = 'mortek-v32';
 const PRECACHE = [
   '/',
   '/index.html',
@@ -8,7 +8,7 @@ const PRECACHE = [
   '/about.html',
   '/blog.html',
   '/404.html',
-  '/style.min.css?v=51',
+  '/style.min.css?v=53',
   '/site.min.js?v=1',
   '/favicon.svg',
   '/manifest.json',
