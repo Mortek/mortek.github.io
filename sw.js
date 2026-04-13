461d3f085f24f12d5f33433d7913260dd11b8f6d
Maurice Moret <mmoret@netvlies.net>
2026-04-13 18:50:40 +0200

    fix: increase specificity on article button color override
    .book-card .buy-btn was overriding .buy-btn--outline due to higher
    specificity. Changed selector to .book-card .buy-btn.buy-btn--outline.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mortek-v35';
+const CACHE = 'mortek-v36';
 const PRECACHE = [
   '/',
   '/index.html',
@@ -8,7 +8,7 @@ const PRECACHE = [
   '/about.html',
   '/blog.html',
   '/404.html',
-  '/style.min.css?v=56',
+  '/style.min.css?v=57',
   '/site.min.js?v=1',
   '/favicon.svg',
   '/manifest.json',
