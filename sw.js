d10332ff15f50bfa0779013761e05abcd43a90ae
Maurice Moret <mmoret@netvlies.net>
2026-04-13 18:59:04 +0200

    style: make featured book buttons equal width (200px)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mortek-v38';
+const CACHE = 'mortek-v39';
 const PRECACHE = [
   '/',
   '/index.html',
@@ -8,7 +8,7 @@ const PRECACHE = [
   '/about.html',
   '/blog.html',
   '/404.html',
-  '/style.min.css?v=59',
+  '/style.min.css?v=60',
   '/site.min.js?v=1',
   '/favicon.svg',
   '/manifest.json',
